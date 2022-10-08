2bdd843957b05d744e0fd153f7ec4574e6066638
VazifaGuliyeva <vezife07102002@gmail.com>
2022-10-08 22:46:29 +0400

    First project

---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student extends Person{
+    private String schoolname;
+    private double scoholorship;
+
+    public String getSchoolname() {
+        return schoolname;
+    }
+
+    public void setSchoolname(String schoolname) {
+        this.schoolname = schoolname;
+    }
+
+    public double getScoholorship() {
+        return scoholorship;
+    }
+
+    public void setScoholorship(double scoholorship) {
+        this.scoholorship = scoholorship;
+    }
+}
---
+++ Teacher.java
@@ -0,0 +1,20 @@
+public class Teacher extends Person{
+    private String schoolname;
+    private double salary;
+
+    public String getSchoolname() {
+        return schoolname;
+    }
+
+    public void setSchoolname(String schoolname) {
+        this.schoolname = schoolname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
